58a8a3e3810dc0a2e723bac48cbe358a482e8b16
OH0LYW4T3R <tjdtnco@gmail.com>
2024-11-03 19:34:41 +0900

    Add Test Page

---
+++ src/main/java/com/example/demo/test/TestController.java
@@ -0,0 +1,14 @@
+package com.example.demo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping("/main")
+    public String testMain() {
+        return "test/test.html";
+    }
+}
